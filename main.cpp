0ad39699c5b7f0834280eda66446c1a6b6b62cbd
Théo Zozime <66619733+zZz-Theo-zZz@users.noreply.github.com>
2024-09-16 16:54:36 +0200

    Add main.cpp and a dependency provided by lesson's author to write TGA files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+int main(int argc, char** argv)
+{
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(0, 0, white);
+    image.set(99, 99, red);
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}
